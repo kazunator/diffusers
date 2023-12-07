9424356e52cb78988dfd3c2e55ed8bd2ddd6513f
omar el amrani <pour2002@gmail.com>
2023-12-07 19:24:35 +0100

    Update train_dreambooth_lora_sdxl.py

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1443,8 +1443,8 @@ def main(args):
                         )
                     else:
                         #this should be a function, but I'm currently just putting here for my own sanity. Clean later                        
-                        tokens_one = compel1.build_conditioning_tensor(prompts)
-                        tokens_two = compel2.build_conditioning_tensor(prompts)
+                        tokens_one = compel1(prompts)
+                        tokens_two = compel2(prompts)
 
                 # Convert images to latent space
                 model_input = vae.encode(pixel_values).latent_dist.sample()
